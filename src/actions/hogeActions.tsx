9b7f8b490f79396e5948cdaff617dd2836026145
sun-yryr <taittide@gmail.com>
2019-11-08 01:27:37 +0900

    a

--- src/actions/hogeActions.tsx
+++
@@ -1,7 +0,0 @@
-import actionCreatorFactory from 'typescript-fsa';
-
-const actionCreator = actionCreatorFactory();
-
-export const hogeActions = {
-    update: actionCreator<string>('ACTIONS_UPDATE'),
-};
