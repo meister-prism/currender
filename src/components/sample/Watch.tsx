5260bc18151eb1d3984aebdff90f17f784ad6c72
sun-yryr <taittide@gmail.com>
2019-11-20 09:34:16 +0900

    styled-componentを書いた

--- src/components/sample/Watch.tsx
+++ src/components/sample/Watch.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 interface Props {
     Date: string,
     Time: string,
 }
 
+/* styled-component の使い方
+    基本タグを選んで（ex: 下はdiv）cssを付け足す
+    function内で変数宣言すると，propsを使ったスタイリングができるけど...？
+*/
+const Root = styled.div`
+    width: 500px
+    margin: 0 auto
+    background-color: #f3f3f3
+    text-align: center
+`;
+
 export default function Watch(props: Props): JSX.Element {
     const { Date, Time } = props;
     return (
-        <div>
+        <Root>
             <p>{Date}</p>
             <p>{Time}</p>
-        </div>
+        </Root>
     );
 }
