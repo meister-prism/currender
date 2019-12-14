00fbd7a4f347ac042d9bd84b048ced201e894fcd
sun-yryr <taittide@gmailcom>
2019-12-14 14:54:36 +0000

    [Add] what_is_today

--- src/actions/CurrentAction.tsx
+++ src/actions/CurrentAction.tsx
@@ -2,16 +2,23 @@ import { Action } from 'redux';
 
 export enum CurrentType {
     UPDATE_DATETIME = 'UPDATE_DATETIME',
+    UPDATE_WHATISTODAY = 'UPDATE_WHATISTODAY',
 }
 
 interface ICurrentDateTimeAction extends Action {
     type: CurrentType.UPDATE_DATETIME,
 }
 
-export type CurrentAction = ICurrentDateTimeAction;
+interface ICurrentWITAction extends Action {
+    type: CurrentType.UPDATE_WHATISTODAY,
+    payload: string,
+}
+
+export type CurrentAction = ICurrentDateTimeAction | ICurrentWITAction;
 
 interface ICurrentActionCreator {
     updateNowDate(): ICurrentDateTimeAction,
+    updateWhatIsToday(value: string): ICurrentWITAction,
 }
 
 
@@ -19,6 +26,11 @@ class CurrentActionCreator implements ICurrentActionCreator {
     public updateNowDate = (): ICurrentDateTimeAction => ({
         type: CurrentType.UPDATE_DATETIME,
     });
+
+    public updateWhatIsToday = (value: string): ICurrentWITAction => ({
+        type: CurrentType.UPDATE_WHATISTODAY,
+        payload: value,
+    })
 }
 
 export const currentActionCreator: ICurrentActionCreator = new CurrentActionCreator();
--- src/reducers/CurrentReducer.tsx
+++ src/reducers/CurrentReducer.tsx
@@ -4,10 +4,12 @@ import { CurrentAction, CurrentType } from '../actions/CurrentAction';
 
 export interface ICurrentState {
     nowDateTime: moment.Moment,
+    whatIsToday: string,
 }
 
 const initState: ICurrentState = {
     nowDateTime: moment('2019-05-01T12:00:00'),
+    whatIsToday: '',
 };
 
 export const CurrentReducer: Reducer<ICurrentState, CurrentAction> = (
@@ -17,8 +19,17 @@ export const CurrentReducer: Reducer<ICurrentState, CurrentAction> = (
     switch (action.type) {
         case CurrentType.UPDATE_DATETIME: {
             const nowMoment: moment.Moment = moment();
+            const { whatIsToday } = state;
             return {
                 nowDateTime: nowMoment,
+                whatIsToday,
+            };
+        }
+        case CurrentType.UPDATE_WHATISTODAY: {
+            const { nowDateTime } = state;
+            return {
+                nowDateTime,
+                whatIsToday: action.payload,
             };
         }
         default: {
