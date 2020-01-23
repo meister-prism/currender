90a37a11c8797bddd0b9c106c6c95e91f5662a8c
214hajime <shajime.0214@gmail.com>
2020-01-23 15:58:23 +0900

    update

--- src/calendars/himekuri/default/components/himekuri.tsx
+++ src/calendars/himekuri/default/components/himekuri.tsx
@@ -11,95 +11,86 @@ interface Props {
 export function TestHimekuri(props: Props) {
     const { title } = props;
     return (
-        <Flexbox>
-            <Gallery>
-                <h1>04</h1>
-                <p>April</p>
-                <h2>レポート締め切り</h2>
-                <h3>14:00-15:00 ミーティング</h3>
-            </Gallery>
-            <Main>
-                <h1>2</h1>
-                <p>THU</p>
-            </Main>
-            <Side>
-                <h1>20</h1>
-                <h2>/</h2>
-                <h3>16</h3>
-                <h4>°C</h4>
-                <h5>今日はCO₂の日です</h5>
-                <h6>[遅延情報]</h6>
-                <p>[News]</p>
-            </Side>
-        </Flexbox>
+        <Root>
+            <Flex>
+                <Gallery>
+                    <h1>04</h1>
+                    <p>April</p>
+                    <h2>レポート締め切り</h2>
+                    <h3>14:00-15:00 ミーティング</h3>
+                </Gallery>
+                <Main>
+                    <h1>2</h1>
+                    <p>THU</p>
+                </Main>
+                <Side>
+                    <h1>20/16°C</h1>
+                    <h2>今日はCO₂の日です</h2>
+                    <h3>[遅延情報]</h3>
+                    <p>[News]</p>
+                </Side>
+            </Flex>
+        </Root>
     );
 }
 
-const Flexbox = styled.div`
-    display:flex;
-    widht:;
-    height:;
+const Root = styled.div`
+    height: 1280px;
+    width: 1080px;
+    margin: 5px;
+`;
+const Flex = styled.div`
+    display: flex;
 `;
 
 const Gallery = styled.div`
     h1{
-        font-size:50px;
-        font-family:;
+        font-size: 50px;
+        font-family: ;
     }
     p{
-        font-size:25px;
-        font-weight:800;
-        font-family:;
+        font-size: 25px;
+        font-weight: 800;
+        font-family: ;
     }
     h2{
-        font-size:20px;
-        font-family:;
+        font-size: 20px;
+        font-family: ;
     }
     h3{
-        font-size:14px;
-        font-weight:200;
-        font-family:;
-        font-family:;
+        font-size: 14px;
+        font-weight: 200;
+        font-family: ;
+        font-family: ;
     }
 `;
 
 const Main = styled.div`
     h1{
-        font-size:200px;
-        font-family:'Gulim',;
+        font-size: 200px;
+        font-family: 'Gulim',;
     }
     p{
-        font-size:30px;
-        color:darkgray;
+        font-size: 30px;
+        color: darkgray;
     }
 `;
 
 const Side = styled.div`
     h1{
-        font-size:25px;
-        color:tomato;
+        font-size: 25px;
+        color: tomato;
     }
     h2{
-        font-size:25px;
+        font-size: 18px;
+        font-weight: 400;
     }
     h3{
-        font-size:25px;
-        color:deepskyblue;
-    }
-    h4{
-        font-size:25px;
-        color:deepskyblue;
-    }
-    h5{
-        font-size:18px;
-        font-weight:400;
-    }
-    h6{
-        font-size:15px;
-        font-weight:200;
+        font-size: 15px;
+        font-weight: 100;
     }
     p{
-        font-size:15px;
-        font-weight:200;
-    }
+        font-size: 15px;
+        font-weight: 100;
+    }    
 `;
