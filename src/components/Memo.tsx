f90cd946efaf5a9efb8fe08700848a3f0455f5ae
Akari000 <app.sub.k@gmail.com>
2020-02-21 15:08:32 +0900

    add view

--- src/components/Memo.tsx
+++ src/components/Memo.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import stackmemo from './logo512.png';
+import stackmemo from '../notes/memo1.jpg';
+import clear from '../assets/clear.png';
+import save from '../assets/save.png';
 
 interface Props {
     onMouseDown: (e: any) => void;
@@ -17,41 +19,40 @@ const Root = styled.div`
 `;
 
 const SaveButton = styled.a`
-    font-size: 10px
-    padding: 4px
-    margin-right: 3px
-    border: solid 1px grey
-    border-radius: 0.2em
-    background-color: white
+    margin-right: 5px
+    width: 40px
+    height: 40px
 `;
 
-const ClearButton = styled.button`
-    font-size: 10px
-    border: solid 1px grey
-    border-radius: 0.2em
+const ClearButton = styled.input`
+    width: 40px
+    height: 40px
 `;
 
 const StackNotes = styled.div`
     text-align: left
 `;
 
-const Notes = styled.div`
+const NoteImg = styled.img`
     display: inline-block 
     width: 230px
     height: 230px
-    margin-right: 5px
-    background-size: cover
-    // background-image: url("./logo512.png")
-    background-color: #FFFFCC
-    border-radius: 0.2em
-    box-shadow: 2px 2px 2px rgba(0,0,0,0.2)
+    margin-right: 20px
+    background-color: #FFF5C2
+    box-shadow: 2px 5px rgba(0,0,0,0.3)
 `;
 
-const canvasStyle = {
-    boxShadow: '5px 5px 5px rgba(240,230,140,1)',
-    marginLeft: '10px',
-    backgroundColor: '#FFFFCC',
+const NoteStyle = {
+    backgroundColor: '#BCDEFE',
 };
+
+const canvasStyle = {
+    boxShadow: '2px 5px rgba(240,230,140,1)',
+    margin: '0 0 10px 10px',
+    backgroundColor: '#FFF5C2',
+};
+
+const UrlList = ['../notes/memo1.jpg', '../notes/memo1.jpg'];
 
 export function Memo(props: Props): JSX.Element {
     const {
@@ -65,11 +66,17 @@ export function Memo(props: Props): JSX.Element {
     return (
         <Root>
             <StackNotes>
-                <Notes />
-                <Notes />
-                <Notes />
+                {UrlList.map((url) => <NoteImg src={url} alt="" />)}
+                <NoteImg src={stackmemo} alt="" />
+                <NoteImg src={stackmemo} style={NoteStyle} alt="" />
             </StackNotes>
             <br />
+            <div>
+                <SaveButton onClick={saveCanvas} id="download" download="sample.jpg">
+                    <img width="40px" src={save} alt="保存" />
+                </SaveButton>
+                <ClearButton onClick={clearCanvas} type="image" src={clear} name="clearButton" alt="取り消し" />
+            </div>
             <canvas
                 width="250px"
                 height="250px"
@@ -80,12 +87,6 @@ export function Memo(props: Props): JSX.Element {
                 onMouseMove={onMouseMove}
                 style={canvasStyle}
             />
-            <div>
-                <SaveButton onClick={saveCanvas} id="download" download="sample.jpg">
-                    決定
-                </SaveButton>
-                <ClearButton onClick={clearCanvas} type="button">取り消し</ClearButton>
-            </div>
         </Root>
     );
 }
